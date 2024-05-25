14e7604e7ac736bffe953a3d7838d9f4ccf8fe48
varuntyagiG <varungauravtyagi@gmail.com>
2024-05-25 23:47:55 +0530

    xyz

--- src/product.jsx
+++ src/product.jsx
@@ -1,13 +1,22 @@
 import "./product.css";
 
-function Product({ title, price, feature }) {
-  return (
-    <div className="Product">
-      <h3>{title}</h3>
-      <h5>Price : {price}</h5>
-      <h5>{feature}</h5>
-    </div>
-  );
+function Product({ title, price }) {
+  if (price > 40000) {
+    return (
+      <div className="Product">
+        <h3>{title}</h3>
+        <h5>Price : {price}</h5>
+        <p>Discount of 5%</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Product">
+        <h3>{title}</h3>
+        <h5>Price : {price}</h5>
+      </div>
+    );
+  }
 }
 
 export { Product };
--- src/productTab.jsx
+++ src/productTab.jsx
@@ -1,10 +1,9 @@
 import { Product } from "./product";
 
 function ProductTab() {
-  let feature = [<li>"good"</li>, <li>"reliable"</li>, <li>"hi-tech"</li>];
   return (
     <>
-      <Product title="phone" price={35000} feature={feature} />
+      <Product title="phone" price={35000} />
       <Product title="laptop" price={70000} />
       <Product title="car" price={1200000} />
     </>
